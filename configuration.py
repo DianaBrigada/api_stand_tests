4f398325736858f760c4a66049c7b05abb0beb1e
Diana Brigada <dianabrigadaflores@gmail.com>
2025-09-02 17:51:03 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://cnt-0a00a648-d8c4-4406-ae10-df2206461f68.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+CREATE_USER_PATH = "/api/v1/users/"
